68e6ae8500ebb13ef21a6a32ce5769433ae7429a
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:23 +0700

    Create ScoresDAO.cs

---
+++ QLSinhVien/QLSinhVien/DAO/ScoresDAO.cs
@@ -0,0 +1,52 @@
+﻿using QLSinhVien.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DAO
+{
+	public class ScoresDAO
+	{
+		private static ScoresDAO instance;
+
+		public static ScoresDAO Instance {
+			get { if(instance == null) instance = new ScoresDAO(); return instance; }
+			set => instance = value; }
+
+		private ScoresDAO() { }
+
+		public List<Scores> GetAllScores()
+		{
+
+			List<Scores> scores = new List<Scores>();
+
+			string query = "select * from Scores";
+			DataTable dt = DataProvider.Instance.ExecuteQuery(query);
+
+			foreach (DataRow row in dt.Rows)
+			{
+				Scores s = new Scores(row);
+				scores.Add(s);
+			}
+
+			return scores;	
+
+	
+		}
+
+		public int CountScore()
+		{
+			int s;
+
+			string query = "select* from Scores";
+			DataTable dt = DataProvider.Instance.ExecuteQuery(query);
+
+			s= dt.Rows.Count;
+
+			return s;
+		}
+	}
+}
