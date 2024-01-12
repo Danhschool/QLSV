aabb3301ca4defab699a77098648114a76a6aa18
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:39 +0700

    Create Major.cs

---
+++ QLSinhVien/QLSinhVien/DTO/Major.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DTO
+{
+	public class Major
+	{
+		public Major(string majorID, string majorName)
+		{
+			this.MajorID = majorID;
+			this.MajorName = majorName;
+		}
+
+		public Major(DataRow row)
+		{
+			this.MajorID = row["MajorID"].ToString();
+			this.MajorName = row["MajorName"].ToString() ;
+		}
+		private string majorID;
+		private string majorName;
+
+		public string MajorID { get => majorID; set => majorID = value; }
+		public string MajorName { get => majorName; set => majorName = value; }
+	}
+}
