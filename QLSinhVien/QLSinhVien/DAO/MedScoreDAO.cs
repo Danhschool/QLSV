14ed13ad8555def4d20122dd872209530ab1a151
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:22 +0700

    Create MedScoreDAO.cs

---
+++ QLSinhVien/QLSinhVien/DAO/MedScoreDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DAO
+{
+	public class MedScoreDAO
+	{
+		private static MedScoreDAO instance;
+
+		public static MedScoreDAO Instance { get
+			{ if (instance == null) instance = new MedScoreDAO(); return instance; } set => instance = value; }
+
+		private MedScoreDAO() { }
+	}
+}
