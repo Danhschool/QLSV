58ea9697554a2326073e5b01e252a8a16b651f2d
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:33 +0700

    Create TeacherDAO.cs

---
+++ QLSinhVien/QLSinhVien/DAO/TeacherDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DAO
+{
+	public class TeacherDAO
+	{
+		private static TeacherDAO instance;
+
+		public static TeacherDAO Instance {
+			get { if(instance == null) instance = new TeacherDAO(); return instance; }
+			set => instance = value; }
+
+		private TeacherDAO() { }	
+	}
+}
