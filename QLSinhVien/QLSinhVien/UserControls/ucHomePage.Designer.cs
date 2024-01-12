c35234efb6eab4a06ce272602becec4d9e24ee90
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:19:32 +0700

    Create ucHomePage.Designer.cs

---
+++ QLSinhVien/QLSinhVien/UserControls/ucHomePage.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace QLSinhVien.UserControls
+{
+	partial class ucHomePage
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel7 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.lblCountMajor = new System.Windows.Forms.Label();
+			this.ptMajor = new System.Windows.Forms.PictureBox();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lblCountSubject = new System.Windows.Forms.Label();
+			this.ptSubject = new System.Windows.Forms.PictureBox();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblCountStudent = new System.Windows.Forms.Label();
+			this.ptStudent = new System.Windows.Forms.PictureBox();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblCountClass = new System.Windows.Forms.Label();
+			this.ptClass = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cboClass = new System.Windows.Forms.ComboBox();
+			this.panel2.SuspendLayout();
+			this.panel7.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptMajor)).BeginInit();
+			this.panel6.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptSubject)).BeginInit();
+			this.panel5.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptStudent)).BeginInit();
+			this.panel4.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptClass)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.panel7);
+			this.panel2.Controls.Add(this.panel6);
+			this.panel2.Controls.Add(this.panel5);
+			this.panel2.Controls.Add(this.panel4);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1525, 150);
+			this.panel2.TabIndex = 1;
+			// 
+			// panel7
+			// 
+			this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel7.Controls.Add(this.label6);
+			this.panel7.Controls.Add(this.lblCountMajor);
+			this.panel7.Controls.Add(this.ptMajor);
+			this.panel7.Location = new System.Drawing.Point(1130, 27);
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new System.Drawing.Size(333, 98);
+			this.panel7.TabIndex = 1;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(221, 51);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(95, 16);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Chuyên Ngành";
+			// 
+			// lblCountMajor
+			// 
+			this.lblCountMajor.AutoSize = true;
+			this.lblCountMajor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCountMajor.Location = new System.Drawing.Point(219, 17);
+			this.lblCountMajor.Name = "lblCountMajor";
+			this.lblCountMajor.Size = new System.Drawing.Size(24, 25);
+			this.lblCountMajor.TabIndex = 7;
+			this.lblCountMajor.Text = "8";
+			// 
+			// ptMajor
+			// 
+			this.ptMajor.Location = new System.Drawing.Point(32, 17);
+			this.ptMajor.Name = "ptMajor";
+			this.ptMajor.Size = new System.Drawing.Size(60, 60);
+			this.ptMajor.TabIndex = 3;
+			this.ptMajor.TabStop = false;
+			// 
+			// panel6
+			// 
+			this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel6.Controls.Add(this.label4);
+			this.panel6.Controls.Add(this.lblCountSubject);
+			this.panel6.Controls.Add(this.ptSubject);
+			this.panel6.Location = new System.Drawing.Point(765, 27);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(331, 98);
+			this.panel6.TabIndex = 1;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(220, 51);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(58, 16);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Môn học";
+			// 
+			// lblCountSubject
+			// 
+			this.lblCountSubject.AutoSize = true;
+			this.lblCountSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCountSubject.Location = new System.Drawing.Point(218, 17);
+			this.lblCountSubject.Name = "lblCountSubject";
+			this.lblCountSubject.Size = new System.Drawing.Size(24, 25);
+			this.lblCountSubject.TabIndex = 5;
+			this.lblCountSubject.Text = "8";
+			// 
+			// ptSubject
+			// 
+			this.ptSubject.Location = new System.Drawing.Point(22, 17);
+			this.ptSubject.Name = "ptSubject";
+			this.ptSubject.Size = new System.Drawing.Size(60, 60);
+			this.ptSubject.TabIndex = 2;
+			this.ptSubject.TabStop = false;
+			// 
+			// panel5
+			// 
+			this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel5.Controls.Add(this.label1);
+			this.panel5.Controls.Add(this.lblCountStudent);
+			this.panel5.Controls.Add(this.ptStudent);
+			this.panel5.Location = new System.Drawing.Point(389, 27);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(329, 98);
+			this.panel5.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(199, 51);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 16);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Sinh viên";
+			// 
+			// lblCountStudent
+			// 
+			this.lblCountStudent.AutoSize = true;
+			this.lblCountStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCountStudent.Location = new System.Drawing.Point(197, 17);
+			this.lblCountStudent.Name = "lblCountStudent";
+			this.lblCountStudent.Size = new System.Drawing.Size(24, 25);
+			this.lblCountStudent.TabIndex = 3;
+			this.lblCountStudent.Text = "8";
+			// 
+			// ptStudent
+			// 
+			this.ptStudent.Location = new System.Drawing.Point(24, 17);
+			this.ptStudent.Name = "ptStudent";
+			this.ptStudent.Size = new System.Drawing.Size(60, 60);
+			this.ptStudent.TabIndex = 1;
+			this.ptStudent.TabStop = false;
+			// 
+			// panel4
+			// 
+			this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel4.Controls.Add(this.label2);
+			this.panel4.Controls.Add(this.lblCountClass);
+			this.panel4.Controls.Add(this.ptClass);
+			this.panel4.Location = new System.Drawing.Point(43, 27);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(323, 98);
+			this.panel4.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(178, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Lớp học";
+			// 
+			// lblCountClass
+			// 
+			this.lblCountClass.AutoSize = true;
+			this.lblCountClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCountClass.Location = new System.Drawing.Point(176, 17);
+			this.lblCountClass.Name = "lblCountClass";
+			this.lblCountClass.Size = new System.Drawing.Size(24, 25);
+			this.lblCountClass.TabIndex = 1;
+			this.lblCountClass.Text = "8";
+			// 
+			// ptClass
+			// 
+			this.ptClass.Location = new System.Drawing.Point(24, 17);
+			this.ptClass.Name = "ptClass";
+			this.ptClass.Size = new System.Drawing.Size(60, 60);
+			this.ptClass.TabIndex = 0;
+			this.ptClass.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.cboClass);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.chart1);
+			this.panel1.Location = new System.Drawing.Point(3, 156);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1519, 590);
+			this.panel1.TabIndex = 2;
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(526, 75);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(846, 512);
+			this.chart1.TabIndex = 1;
+			this.chart1.Text = "chart1";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(37, 51);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(72, 20);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Chọn lớp";
+			// 
+			// cboClass
+			// 
+			this.cboClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cboClass.FormattingEnabled = true;
+			this.cboClass.Location = new System.Drawing.Point(41, 88);
+			this.cboClass.Name = "cboClass";
+			this.cboClass.Size = new System.Drawing.Size(306, 28);
+			this.cboClass.TabIndex = 3;
+			// 
+			// ucHomePage
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panel2);
+			this.Name = "ucHomePage";
+			this.Size = new System.Drawing.Size(1525, 749);
+			this.Load += new System.EventHandler(this.ucHomePage_Load);
+			this.panel2.ResumeLayout(false);
+			this.panel7.ResumeLayout(false);
+			this.panel7.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptMajor)).EndInit();
+			this.panel6.ResumeLayout(false);
+			this.panel6.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptSubject)).EndInit();
+			this.panel5.ResumeLayout(false);
+			this.panel5.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptStudent)).EndInit();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptClass)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel7;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label lblCountMajor;
+		private System.Windows.Forms.PictureBox ptMajor;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblCountSubject;
+		private System.Windows.Forms.PictureBox ptSubject;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblCountStudent;
+		private System.Windows.Forms.PictureBox ptStudent;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblCountClass;
+		private System.Windows.Forms.PictureBox ptClass;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ComboBox cboClass;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+	}
+}
