672fdb3a577cf7985f0bf4ace02f8951c3d8cf9b
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:31 +0700

    Create SubjectDAO.cs

---
+++ QLSinhVien/QLSinhVien/DAO/SubjectDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DAO
+{
+	public class SubjectDAO
+	{
+		private static SubjectDAO instance;
+
+		public static SubjectDAO Instance
+		{
+			get { if(instance == null) instance = new SubjectDAO(); return instance; }
+			set => instance = value; }
+		private SubjectDAO() { }
+
+		public int CountSubject()
+		{
+			int c;
+
+			string query = "select * from Subject";
+			DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+			c = data.Rows.Count;
+
+			return c;
+		}
+	}
+}
