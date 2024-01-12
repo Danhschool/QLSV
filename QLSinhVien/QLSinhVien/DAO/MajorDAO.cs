02fbdf4662f848d196e5f96e35bdee85c9945fcc
Danhschool <116479213+Danhschool@users.noreply.github.com>
2024-01-12 14:18:20 +0700

    Create MajorDAO.cs

---
+++ QLSinhVien/QLSinhVien/DAO/MajorDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSinhVien.DAO
+{
+	public class MajorDAO
+	{
+		private static MajorDAO instance;
+
+		public static MajorDAO Instance
+		{
+			get { if(instance == null) instance = new MajorDAO(); return instance; }
+			set => instance = value; }
+
+		private MajorDAO() { }
+
+		public int CountMajor()
+		{
+			int c;
+			string query = "select * from Major";
+			DataTable dt = DataProvider.Instance.ExecuteQuery(query);
+
+			c = dt.Rows.Count;
+
+			return c;
+		}
+	}
+}
